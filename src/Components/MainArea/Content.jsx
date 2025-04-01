d492b2047223dfc6dfe40f371ebd5fa5769be7be
John Rogee Turqueza <jrogeet@gmail.com>
2025-04-02 00:16:53 +0800

    wip

--- src/Components/MainArea/Content.jsx
+++ src/Components/MainArea/Content.jsx
@@ -3,7 +3,7 @@ import Creations from './Sections/Creations';
 
 export default function Content() {
     return (
-        <div className="w-full h-full bg-offwhite rounded-b-xl shadow-inner shadow-black/25">
+        <div className="w-full h-full bg-offwhite rounded-b-xl shadow-inner shadow-black/25 overflow-y-auto">
             <div className="w-full flex flex-col items-center gap-10">
                 <Hero />
                 <Creations />
--- src/Components/MainArea/Sections/Creations.jsx
+++ src/Components/MainArea/Sections/Creations.jsx
@@ -1,7 +1,16 @@
 export default function Creations() {
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 items-center">
             <h1 className="font-megren text-6xl text-coalblack">my creations</h1>
+
+            <div className="flex">
+                <img src="/images/meme/difficulty.jpg" className="h-40"/>
+                <img src="/images/meme/DANGER.jpg" className="h-40"/>
+                <img src="/images/meme/TT.jpg" className="h-40"/>
+                <img src="/images/meme/WIP.jpg" className="h-40"/>
+            </div>
         </div>
+
+
     );
 }
